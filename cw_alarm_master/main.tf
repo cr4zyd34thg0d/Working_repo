a6a627486c63781a1ee9866a7e6855330066cf61
Devon Diffie <58093002+cr4zyd34thg0d@users.noreply.github.com>
2022-12-11 13:21:31 -0700

    Update main.tf

--- cw_alarm_master/main.tf
+++ cw_alarm_master/main.tf
@@ -1,12 +1,12 @@
 module "log_metric_filter" {
   source = "./modules/log-metric-filter" #Source Location
 
-  log_group_name = "aws-controltower/CloudTrailLogs" #Log Group Nmae
+  log_group_name = "Log_Group_Name" #Log Group Nmae
 
   name    = "error-metric" #Error Metric name
   pattern = "ERROR"
 
-  metric_transformation_namespace = "auction" #Metric namespace
+  metric_transformation_namespace = "metric_name" #Metric namespace
   metric_transformation_name      = "ErrorCount"
 
 }
@@ -14,7 +14,7 @@ module "log_metric_filter" {
 module "log_stream" {
   source = "./modules/log-stream" #source Location
 
-  name           = "auction-stream"                  #Log Stream Name
+  name           = "stream"                  #Log Stream Name
   log_group_name = "aws-controltower/CloudTrailLogs" #Log Group Name
 
 }
@@ -22,19 +22,19 @@ module "log_stream" {
 module "metric_alarm" {
   source = "./modules/metric-alarm" #Source Location
 
-  alarm_name          = "auction-errors" #Metric Alarm Name
-  alarm_description   = "auction alarms" #Alarm Descritpion
+  alarm_name          = "errors" #Metric Alarm Name
+  alarm_description   = "alarms" #Alarm Descritpion
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1  #How many times to evaluate the alarm
   threshold           = 1  #How many findings is allowed
   period              = 60 #period of 60 seconds
   unit                = "Count"
 
-  namespace   = "auction" #Namespace
+  namespace   = "Name" #Namespace
   metric_name = "ErrorCount"
   statistic   = "Maximum"
 
-  alarm_actions = ["arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:auction-cw-alarm"]
+  alarm_actions = ["arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:SNSname"]
 
   depends_on = [
     module.sns_topic
@@ -43,10 +43,10 @@ module "metric_alarm" {
 
 module "cis_alarms" {
   source    = "./modules/cis-alarms" #Source Location
-  namespace = "auction-cw-alarm"     #Namespace for Alarms
+  namespace = "name-cw-alarm"     #Namespace for Alarms
 
-  log_group_name = "aws-controltower/CloudTrailLogs" #log group name
-  alarm_actions  = ["arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:auction-cw-alarm"]
+  log_group_name = "Log_group_Name" #log group name
+  alarm_actions  = ["arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:SNSname"]
 
   depends_on = [
     module.sns_topic
@@ -55,7 +55,7 @@ module "cis_alarms" {
 
 module "sns_topic" {
   source     = "./modules/sns-topic" #Source Location
-  sns_name   = "auction-cw-alarm"    #SNS topic name
+  sns_name   = "Name-cw-alarm"    #SNS topic name
   account_id = data.aws_caller_identity.current.account_id
   protocol   = "email" #Security Email address
 }
