811aaa964404e143f8ae27d923f3250d034c3450
Devon Diffie <58093002+cr4zyd34thg0d@users.noreply.github.com>
2022-12-11 13:24:10 -0700

    Update config_rule.tf

--- config_rule_template/config_rule.tf
+++ config_rule_template/config_rule.tf
@@ -2,12 +2,12 @@ module "cloudtrail_enable" {
   source                 = "./modules/config_rule/EnableCloudTrail" #Update location for module
   config_name            = "cloudtrail_enable"                      #Update Config Rule Name
   config_role_arn        = ""
-  config_role_name       = "auction_config_role"                #Update with Config role name
+  config_role_name       = "Role_name"                #Update with Config role name
   config_aggregator_name = "config_aggregator"                  #Update Config Aggregator Name
   config_recorder_name   = "config_recorder"                    #Update with recorder name
   config_delivery_name   = "config_delivery"                    #Update with recorder name
   enable_config_recorder = true                                 #Update Boolean true/false
-  config_logs_bucket     = "auction-config-log-test-test-12345" #Update update for name of the bucket name for logging
+  config_logs_bucket     = "S3Bucket" #Update update for name of the bucket name for logging
   check_config           = true                                 #Update Boolean true/false
   rule_source            = "CLOUD_TRAIL_ENABLED"                #Update with the rule name to apply
 }
@@ -16,12 +16,12 @@ module "cloudtrail_encryption" {
   source                 = "./modules/config_rule/CloudTrailEncryption" #Update location for module
   config_name            = "cloudtrail_encryption"                      #Update Config Rule Name
   config_role_arn        = ""
-  config_role_name       = "auction_config_role"                #Update with Config role name
+  config_role_name       = "Role_name"                #Update with Config role name
   config_aggregator_name = "config_aggregator"                  #Update Config Aggregator Name
   config_recorder_name   = "config_recorder"                    #Update with recorder name
   config_delivery_name   = "config_delivery"                    #Update with recorder name
   enable_config_recorder = true                                 #Update Boolean true/false
-  config_logs_bucket     = "auction-config-log-test-test-12345" #Update update for name of the bucket name for logging
+  config_logs_bucket     = "S3Bucket" #Update update for name of the bucket name for logging
   check_config           = true                                 #Update Boolean true/false
   rule_source            = "CLOUD_TRAIL_ENCRYPTION_ENABLED"     #Update with the rule name to apply
 }
